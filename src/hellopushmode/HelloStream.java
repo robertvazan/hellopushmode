b33137d7df1b141097eb931320f568e9078b82bd
Robert Vazan <robertvazan@users.noreply.github.com>
2015-12-10 15:12:52 +0100

    Hello World template

--- src/hellopushmode/HelloStream.java
+++ src/hellopushmode/HelloStream.java
@@ -1,4 +1,29 @@
 package hellopushmode;
 
-public class HelloStream {
+import pushmode.dom.DomElement;
+import pushmode.html.Html;
+import pushmode.server.PushStream;
+
+public class HelloStream extends PushStream {
+	@Override public DomElement document() {
+		return Html.html()
+			.add(Html.head()
+				.add(Html.link()
+					.rel("stylesheet")
+					.type("text/css")
+					.href("https://pushmode.angeloflogic.com/styles/hello.css")
+				)
+				.setupPushMode()
+				.add(Html.title()
+					.text("Hello World")
+				)
+			)
+			.add(Html.body()
+				.add(Html.div()
+					.add(Html.p()
+						.text("Hello World!")
+					)
+				)
+			);
+	}
 }
