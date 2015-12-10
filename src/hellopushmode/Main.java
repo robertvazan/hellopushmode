3b29d05ea2e90c11e34caf0a7a0cad2afa057c2f
Robert Vazan <robertvazan@users.noreply.github.com>
2015-12-10 15:13:09 +0100

    Method main()

--- src/hellopushmode/Main.java
+++ src/hellopushmode/Main.java
@@ -1,6 +1,13 @@
 package hellopushmode;
 
+import pushmode.server.PushServer;
+
 public class Main {
 	public static void main(String[] args) {
+		PushServer
+			.login("beta-till-dec31")
+			.listen(8080)
+			.map("/", HelloStream::new)
+			.run();
 	}
 }
