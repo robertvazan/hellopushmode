e6c2b6a5ccd567118fb4142ded305dfe810aa681
Robert Vazan <robertvazan@users.noreply.github.com>
2016-02-19 12:41:17 +0100

    Port 8080 is the default

--- src/hellopushmode/Main.java
+++ src/hellopushmode/Main.java
@@ -6,15 +6,13 @@ public class Main {
 	// PushMode politely leaves main() method in our hands.
 	public static void main(String[] args) {
 		// PushMode server is a mere library.
+		// It listens on port 8080 by default.
 		PushServer
 			// Login to PushMode cloud. We will need this later.
 			.login("beta-till-mar31")
-			// PushMode is an application server, not a generic web server.
-			// Let nginx take port 80/443 and proxy connections to 8080.
-			.listen(8080)
 			// Map our only page to the root URL.
 			.map("/", HelloPage::new)
-			// Run until we get killed.
+			// Run until the process is killed.
 			.run();
 	}
 }
